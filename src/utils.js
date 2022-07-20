b27d8aac2751f092a932bceb3b3741ceff6acf50
EVEILLARD Clément <clement.eveillard@ext.soprasteria.com>
2022-07-20 11:05:22 +0200

    first 3 steps

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+export const repeat = (n) => Array.from(Array(n).keys());
+
+export function getMultiSelected(target) {
+    return Array.from(target.options).reduce((acc, {selected, value}) => {
+        if (selected) {
+            acc.push(value)
+        }
+        return acc;
+    }, [])
+}
+
+const SEC = 1000;
+const MIN = 60 * SEC;
+const HOUR = 60 * MIN;
+const DAY = 24 * HOUR;
+
+export function timestampToDays(ms) {
+    return ms / DAY;
+}
+
+export function generateId() {
+    return Date.now().toString(10);
+}
+
+export function formatedDateInThirtyDays() {
+    return new Date(new Date().setDate(new Date().getDate() + 30)).toISOString().split('T')[0]
+}
